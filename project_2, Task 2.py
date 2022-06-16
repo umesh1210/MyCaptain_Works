8f2c853ca89a42509d5639ea035323a46aebede1
umesh1210 <107026010+umesh1210@users.noreply.github.com>
2022-06-16 16:43:56 +0530

    Add files via upload

---
+++ project_2, Task 2.py
@@ -0,0 +1,14 @@
+
+
+# project 2
+# Task 2
+
+# Python program to print all positive numbers in a Range
+
+n=int(input("enter a number:"))
+a=range(0,n)
+
+for num in a:
+
+    if num >= 0:
+        print(num, end=" ")
