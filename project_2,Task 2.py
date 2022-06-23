82333653e0001a891f3b7d3d205f38f5bacdbb81
umesh1210 <107026010+umesh1210@users.noreply.github.com>
2022-06-23 19:57:03 +0530

    Add files via upload

---
+++ project_2,Task 2.py
@@ -0,0 +1,19 @@
+# project 2
+# Task 2:
+# Write a program to print all positive numbers in a range
+
+
+#a
+input_list1=[12,-7,5,64,-14]
+for int in input_list1:
+    if int>=0:
+        print(int,end=" ",)
+        
+
+
+#b
+input_list2=[12,14,-94,3]
+for int in input_list2:
+
+    if int>=0:
+        print(int,end=" ",)
